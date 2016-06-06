9bd3ecb999ca0e57c96337bb4426f51c539f323e
prussian <generalunrest@airmail.cc>
2016-06-05 23:38:18 -0400

    Revert "used other irc client written using ncat or bash's /dev/tcp"
    This reverts commit 1f349380f3d15831327adc991710dbf301c4fc1c.

--- config.sh
+++ config.sh
@@ -2,16 +2,13 @@
 # server
 server="irc.rizon.net"
 nickname="the8ball"
-port="6697"
+port="6667"
 # space sep list of chans
-channels=()
+channels=('#chan1' '#chan2' '#etc')
 # 8ball responses
 t8ball="8ball-resp.txt"
 infile="/tmp/in-8ballbot"
 outfile="/tmp/out-8ballbot"
-# TLS
-# if enabled set it to --tls -t or --ssl
-TLS="-t"
 # logging
 # leave blank for no logging
 logfile="convo.log"
