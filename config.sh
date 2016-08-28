a2a181888f48747a836bd1f63145432a8c5dfc95
adedomin <anthony@dedominic.pw>
2016-08-28 00:03:16 -0400

    changed channel config to a list

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ nickname="magic8ball"
 port="6697"
 ssl="true"
 # space sep list of chans
-channels="#chan1 #chan2 #chanetc"
+channels=('#chan1' '#chan2' '#chanetc')
 infile="/tmp/in-8ballbot"
 outfile="/tmp/out-8ballbot"
 # files
